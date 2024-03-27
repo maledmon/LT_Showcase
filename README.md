fe5234e32387bdde352299d6835faa367bedf842
Matt Edmonds <matt@ecp123.com>
2024-03-27 10:37:49 -0400

    Update Readme for testing, and add a few more tests.

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 3. Run "yarn" to pull in all of the packages required
 4. Run "yarn dev" to start up the server
 5. In a web browser navigate to "localhost:3000"
+
+## Running Automated tests
+1. run "yarn dev" to start up the server
+2. run "yarn run cypress open" to kick off the automated tests
+3. Click E2E Testing
+4. Choose browser and click "Start E2E Testing in ..."
+5. choose "cypress\e2e\spec.cy.ts" to run the tests
--- lt-showcase/cypress/e2e/spec.cy.ts
+++ lt-showcase/cypress/e2e/spec.cy.ts
@@ -19,4 +19,24 @@ describe('Home Page', () => {
     cy.wait(2000)
     cy.get('.DialogCloseButton ').click()
   })
+
+  it('changes the number of results on page depending on what is selected', () => {
+    cy.wait(2000)
+    cy.get('.MuiCard-root').should('have.length', 10)
+    cy.get('#results-per-page-select').click();
+    cy.get('.MuiButtonBase-root:nth-child(2)').click();
+    cy.wait(1000)
+    cy.get('.MuiCard-root').should('have.length', 25)
+    cy.get('#results-per-page-select').click();
+    cy.get('.MuiButtonBase-root:nth-child(3)').click();
+    cy.wait(1000)
+    cy.get('.MuiCard-root').should('have.length', 50)
+  })
+
+  it('searches and displays 2 results when searching for "test"', () => {
+    cy.get('#search-input').click();
+    cy.get('#search-input').type('test');
+    cy.get('button[aria-label="Search"]').click();
+    cy.get('.MuiCard-root').should('have.length', 2)
+  })
 })
